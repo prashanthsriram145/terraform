205ee47cb7317dccdf0483c646f181667c157ad7
Prashanth Sriram <prashanthkumar.sriram@blueyonder.com>
2020-11-06 20:16:34 +0530

    Adding first terraform to create aws ec2 instance

---
+++ first/createEC2Instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my-first-instance" {
+  ami = "ami-0947d2ba12ee1ff75"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
