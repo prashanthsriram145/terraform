490d078c0306e5c0b01c3cd377856932dc6f9be4
Prashanth Sriram <prashanthkumar.sriram@blueyonder.com>
2020-11-07 10:48:15 +0530

    Included changes for web server, variables and ouput

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # terraform
+
+- terraform plan - To check what will terraform script would do if ran.
+
+- terraform apply - To apply changes as per the terraform script.
+
+- terraform destroy - To destroy all resources created by terraform script.
+
+- terraform output - To display output variables.
--- first/createEC2Instance.tf
+++ first/createEC2Instance.tf
@@ -3,11 +3,47 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-first-instance" {
-  ami = "ami-0947d2ba12ee1ff75"
+  ami = "ami-0817d428a6fb68645"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+
+  user_data = <<-EOF
+              !#/bin/bash
+              echo "Hello World!" >> index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-example"
   }
 }
 
+resource "aws_security_group" "security_group" {
+
+  name = "terraform-security-group"
+
+  ingress {
+    from_port = var.server_port
+    protocol = "tcp"
+    to_port = var.server_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "server port"
+  default = 8080
+  type = number
+}
+
+output "public_ip" {
+  value = aws_instance.my-first-instance.public_ip
+  description = "public ip address"
+}
